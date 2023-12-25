9bb8d66066e12270b37abc1c66e809819532c8d5
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2023-12-25 11:06:31 +0000

    feat: server init

--- server/app.js
+++ server/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(PORT, (error) => {
-  if (!error) {
-    console.log(
-      'Server is Successfully Running, and App is listening on port ' + PORT
-    );
-  } else {
-    console.log("Error occurred, server can't start", error);
-  }
+mongoose
+  .connect(
+    'mongodb+srv://' +
+      process.env.LOGIN +
+      ':' +
+      process.env.PASSWORD +
+      process.env.DATABASE,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.error('Error in connection:', err);
+  });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Our server is running on port ${process.env.PORT}`);
 });
